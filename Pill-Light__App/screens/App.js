eb0e798e996cd0894d36d8703098cf1b67d4b5aa
SunwooJaeho <118053865+SunwooJaeho@users.noreply.github.com>
2023-05-02 21:28:39 +0900

    Setting

---
+++ Pill-Light__App/screens/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>우리 팀 모두 화이팅!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
