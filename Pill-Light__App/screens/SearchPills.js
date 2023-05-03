ec29790a0d718d995e19e34a8861bae2532ed18b
SunwooJaeho <118053865+SunwooJaeho@users.noreply.github.com>
2023-05-03 13:27:01 +0900

    feat: SearchPills.js 추가

---
+++ Pill-Light__App/screens/SearchPills.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const SearchPills = () => {
+  return (
+    <View style={styles.container}>
+      <View></View>
+      <View>
+        <Text>SearchPills</Text>
+      </View>
+      <View></View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundcolor: "#fff",
+  },
+});
+
+export default SearchPills;
