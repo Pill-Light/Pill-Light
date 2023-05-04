b3cdb23040650b016f5a524cf6916abcf8f985eb
m2na7 <minha3082@naver.com>
2023-05-04 18:25:04 +0900

    feat: ID,PW 입력 및 로그인 버튼 추가

--- Pill-Light__App/src/screens/Login.js
+++ Pill-Light__App/src/screens/Login.js
@@ -1,18 +1,106 @@
 import { StatusBar } from 'expo-status-bar';
-import { Dimensions, ImageBackground, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Dimensions, ImageBackground, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import React from 'react';
-// import styles from "./css/styles";
 
 const Login = () => {
     return (
         <SafeAreaView>
             <View>
-                <View>
-                    <Text>Login here!</Text>
+                <View style={styles.titleText}>
+                    <Text style={{
+                        fontSize: 44,
+                        fontWeight: "bold",
+                        marginBottom: "2%"
+                        }}
+                    >
+                        로그인
+                    </Text>
+                </View>
+                <View style={{
+                    marginVertical: "10%",
+                    }}
+                >
+                    <TextInput
+                        placeholder='아이디'
+                        placeholderTextColor="#57C5B6" 
+                        style={styles.textInput}   
+                    >        
+                    </TextInput>
+                    <TextInput
+                        placeholder='비밀번호'
+                        placeholderTextColor="#57C5B6"
+                        style={styles.textInput}
+                    >
+                    </TextInput>
+                </View>
+                <View style={styles.subContainer}>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate("Login")}
+                        style={styles.logintBtn}
+                    >
+                        <Text style={styles.login}>로그인 하기</Text>
+                    </TouchableOpacity>
                 </View>
             </View>
         </SafeAreaView>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: "30%",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    subContainer: {
+        marginTop: "15%",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    titleText: {
+        marginTop: "15%",
+        marginBottom: "8%",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    login: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "white",
+    },
+    sign: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "#57C5B6",
+    },
+    logintBtn: {
+        backgroundColor: "#57C5B6",
+        width: "90%",
+        height: 60,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 10,
+    },
+    signBtn: {
+        marginTop: "6%",
+        backgroundColor: "#fafafa",
+        width: "90%",
+        height: 60,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 10,
+    },
+    textInput: {
+        fontSize: 18,
+        width: "90%",
+        backgroundColor: "#fafafa",
+        padding: "6%",
+        margin: "5%",
+        borderRadius: 10,
+        marginVertical: "3%",
+    },
+});
 
 export default Login
