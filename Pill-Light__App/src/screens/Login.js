3eaf05da9c56eec7f0f3ff988d638bfc71eaf473
m2na7 <minha3082@naver.com>
2023-05-04 17:06:28 +0900

    feat: Login.js 추가

---
+++ Pill-Light__App/src/screens/Login.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import { Dimensions, ImageBackground, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+// import styles from "./css/styles";
+
+const Login = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <View>
+                    <Text>Login here!</Text>
+                </View>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default Login
