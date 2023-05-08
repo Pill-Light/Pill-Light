93359f1ff6bdf2fccffab6b116e60196d31c5faf
YJYJ1 <127469107+YJYJ1@users.noreply.github.com>
2023-05-08 23:13:06 +0900

    feat.버튼기능 추가 및 레이아웃 추가

--- Pill-Light__App/src/screens/MainPage.js
+++ Pill-Light__App/src/screens/MainPage.js
@@ -2,8 +2,6 @@ import { Image, TouchableOpacity } from "react-native";
 import { TextInput } from "react-native";
 import { StyleSheet, Text, View, ScrollView } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
-import SearchTabBar from "../components/Search/SearchTabBar";
-import SearchResult from "../components/Search/SearchResult";
 
 const MainPage = () => {
   return (
@@ -15,12 +13,36 @@ const MainPage = () => {
             placeholderTextColor="grey"
             style={styles.inputText}
           ></TextInput>
-          {/* <TouchableOpacity style={styles.searchButton}>
-              <FontAwesome name="search" size={56} color="#57C5B6" />
-            </TouchableOpacity> */}
         </View>
-        <View style={styles.body}></View>
-        <View style={styles.footer}></View>
+        <View style={styles.body}>
+          <View style={styles.page}>
+            <View style={styles.list}></View>
+            <View style={styles.buttonbox}>
+              <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>복용완료!</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.footer}>
+          <TouchableOpacity style={styles.camera}>
+            <FontAwesome
+              style={styles.pic}
+              name="camera"
+              size={80}
+              color="black"
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.mypage}>
+            <FontAwesome
+              style={styles.pic}
+              name="users"
+              size={75}
+              color="#57C5B6"
+            />
+          </TouchableOpacity>
+        </View>
       </View>
     </>
   );
@@ -31,18 +53,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: "4%",
     borderBottomColor: "grey",
   },
-  searchButton: {
-    alignSelf: "flex-end",
-    marginBottom: "4%",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-  },
-  logo: {
-    flex: 0.4,
-  },
+
   inputText: {
     fontSize: 18,
     width: "50%",
@@ -58,10 +69,79 @@ const styles = StyleSheet.create({
   body: {
     flex: 6,
   },
+  page: {
+    flex: 4,
+    width: "80%",
+    height: "30%",
+    marginLeft: "10%",
+    marginRight: "10%",
+    marginBottom: "8%",
+    marginTop: "8%",
+    padding: "2%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: "3%",
+    borderColor: "#57C5B6",
+    borderRadius: 10,
+  },
+  list: {
+    flex: 3,
+    margin: "5%",
+    width: "90%",
+    height: "20%",
+    padding: "2%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: "3%",
+    borderColor: "#57C5B6",
+    borderRadius: 10,
+  },
+  buttonbox: {
+    width: "100%",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  button: {
+    backgroundColor: "#57C5B6",
+    width: "90%",
+    height: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
   footer: {
     flex: 1,
     borderTopWidth: "4%",
     borderTopColor: "grey",
+    flexDirection: "row",
+  },
+  camera: {
+    flex: 1,
+    width: "50%",
+  },
+  mypage: {
+    flex: 1,
+    width: "50%",
+  },
+  pic: {
+    alignSelf: "center",
+    margin: "2%",
+    padding: "5%",
+    color: "black",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
   },
 });
 export default MainPage;
