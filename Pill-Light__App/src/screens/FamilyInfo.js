22cab60c6fb4c651e6be7015983692aa0c37eac1
se0kcess <kimminsuk051@gmail.com>
2023-05-10 18:42:43 +0900

    design:버튼추가

--- Pill-Light__App/src/screens/FamilyInfo.js
+++ Pill-Light__App/src/screens/FamilyInfo.js
@@ -7,16 +7,19 @@ const FamilyInfo = () =>{
       <View style = {styles.container}>
         <View style = {styles.header}>
           <View style = {styles.row1}> 
-            <AntDesign name="bars" size={40} color="gray" />
-            <AntDesign name="serach1" size={40} color="gray" />
+            <AntDesign name="bars" size={40} marginTop= {30} color="gray" />
+            <AntDesign name="search1" size={40} marginTop= {30} color="gray" />
             <Text style = {styles.guestName}>이성진님</Text>
           </View>
           <View style = {styles.row2}>
           <AntDesign name="team" size={50} color="white" />
-            <AntDesign name="idcard" size={50} color="gray" />
+          <AntDesign name="idcard" size={50} color="gray" />
           </View>
         </View>
         <View style = {styles.body}>
+          <View style = {styles.familyLine}>
+            <Text style = {styles.familyName}>이숙자 (32세) 자녀</Text>
+          </View>
         </View>
         <View style = {styles.footer}>
           <AntDesign name="home" size={60} color="white" />
@@ -37,23 +40,44 @@ const FamilyInfo = () =>{
     row1:{
       flex: 1,
       flexDirection: "row",
+      marginTop: 20,
     },
     guestName:{
       color:"white",
-      fontSize: 40,
+      fontSize: 20,
+      fontWeight: 600,
+      marginTop: 40,
+    },
+    guestInFo:{
+      color:"white",
+      fontSize: 20,
       fontWeight: 600,
       alignItems: "center",
+      marginLeft: 10,
+      marginTop: 10,
     },
     row2:{
       flex:1,
       flexDirection: "row",
+      marginTop: 20,
     },
     body: {
       flex: 6,
       backgroundColor: "white", 
     },
+    familyLine:{
+      flex: 0.18,
+      backgroundColor: "#159895",
+    },
+    FamilyName:{
+      color: "white",
+      fontSize: 100,
+      fontWeight: 600,
+      alignItems: "center",
+      marginTop: 40,
+    },
     footer:{
-      marginTop: "15%",
+      marginTop: 10,
       flex: 1.2,
       backgroundColor: "#57C5B6",
       alignItems: "center",
