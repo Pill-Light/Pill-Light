1971d7f8262666f21476aef34dce102b5bdd80f6
se0kcess <kimminsuk051@gmail.com>
2023-05-12 16:41:03 +0900

    "design : 레이아웃 변경"

--- Pill-Light__App/src/screens/FamilyInfo.js
+++ Pill-Light__App/src/screens/FamilyInfo.js
@@ -1,39 +1,47 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity } from "react-native";
 import { AntDesign } from '@expo/vector-icons'; 
+import SearchTabBar from "../components/Search/SearchTabBar";
+import SearchResult from "../components/Search/SearchResult";
+import { FontAwesome } from "@expo/vector-icons";
+
 const FamilyInfo = () =>{
   return(
       <View style = {styles.container}>
         <View style = {styles.header}>
           <View style = {styles.row1}> 
-            <AntDesign name="bars" size={40} marginTop= {30} color="gray" />
-            <AntDesign name="search1" size={40} marginTop= {30} color="gray"  />
+            <TouchableOpacity style={styles.searchButton}>
+              <FontAwesome name="search" size={40} marginTop={30} color="#57C5B6" />
+            </TouchableOpacity>
             <Text style = {{
-               color:"white",
+               color:"black",
                fontSize: 20,
-               fontWeight: 600,
+               fontWeight: "bold",
                marginTop: 40, 
               }}>
-                이성진님
+                이성진님              68세(여)
             </Text>
           </View>
           <View style = {styles.row2}>
-          <AntDesign name="team" size={50} color="white" />
-          <AntDesign name="idcard" size={50} color="gray" />
+          <AntDesign name="team" size={40} color="#57C5B6" />
+          <AntDesign name="idcard" size={40} color="gray" />
           </View>
         </View>
         <View style = {styles.body}>
           <View style = {styles.familyLine}>
-            <AntDesign name="pluscircleo" size={30} color="black" />
             <Text style = {styles.familyName}>이숙자 (32세) 자녀</Text></View>
-          <View style = {styles.familyline2}>
-            <AntDesign name="pluscircleo" size={30} color="black" />
-            <Text style = {styles.addFamily}>가족 추가하기</Text>
-          </View>
+            <View style={styles.subContainer}>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate("Login")}
+                        style={styles.addBtn}
+                    >
+                        <Text style={styles.add}>가족추가하기</Text>
+                    </TouchableOpacity>
+                </View> 
         </View>
-        <View style = {styles.footer}>
-          <AntDesign name="home" size={60} color="white" />
+        <View style={styles.footer}>
+          <SearchTabBar />
         </View>
       </View>
     );
@@ -46,15 +54,23 @@ const FamilyInfo = () =>{
         },
     header:{
       flex: 2,
-      backgroundColor: "#57C5B6",
+      backgroundColor: "white",
     },
     row1:{
       flex: 1,
       flexDirection: "row",
       marginTop: 20,
     },
+    searchButton: {
+      alignSelf: "flex-end",
+      marginBottom: "4%",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      }},
     guestName:{
-      color:"white",
+      color:"black",
       fontSize: 20,
       fontWeight: 600,
       marginTop: 40,
@@ -94,18 +110,34 @@ const FamilyInfo = () =>{
       flexDirection: "row",
       backgroundColor: "white",
     },
-    addFamily:{
-      color:"black",
-      fontSize: 20,
-      fontWeight: 600,
-      alignItems: "center", 
-    },
-    footer:{
-      marginTop: 10,
-      flex: 1.2,
-      backgroundColor: "#57C5B6",
+    subContainer: {
+      marginTop: "15%",
+      flex: 1,
+      justifyContent: "center",
       alignItems: "center",
-    }
-    
+    },
+    addBtn: {
+      backgroundColor: "#159895",
+      width: "100%",
+      height: 50,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 300,
+      borderRadius: 10,
+    },
+    add: {
+      fontSize: 24,
+      fontWeight: "bold",
+      color: "white",
+  },  
+    footer:{
+      flex: 1.1,
+      backgroundColor: "#fff",
+      flexDirection: "row",
+      paddingBottom: "7%",
+      paddingTop: "5%",
+      borderTopWidth: "4%",
+      borderColor: "#57C5B6",
+    },
   });
   export default FamilyInfo;
