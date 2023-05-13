128e808715ad9c27e0d64e158e939a644e217128
se0kcess <kimminsuk051@gmail.com>
2023-05-13 17:47:41 +0900

    "design : 가족삭제버튼 및 뒤로가기 버튼 추가"

--- Pill-Light__App/src/screens/FamilyInfo.js
+++ Pill-Light__App/src/screens/FamilyInfo.js
@@ -1,10 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, TextInput } from "react-native";
-import { AntDesign } from '@expo/vector-icons'; 
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, ImageBackground } from "react-native";
 import SearchTabBar from "../components/Search/SearchTabBar";
-import SearchResult from "../components/Search/SearchResult";
 import { FontAwesome } from "@expo/vector-icons";
+import { MaterialIcons } from '@expo/vector-icons';
 
 const FamilyInfo = () =>{
   return(
@@ -12,7 +11,10 @@ const FamilyInfo = () =>{
       <View style = {styles.container}>
         <View style = {styles.header}> 
           <View style={styles.searchBar}>
-            <Text style = {styles.guestName}>이성진님     (68세)</Text>
+            <TouchableOpacity style={styles.backButton}>
+              <MaterialIcons name="arrow-back" size={40} color="#159895" />
+            </TouchableOpacity>
+            <Text style = {styles.guestName}>이성진님           (68세)</Text>
             <TouchableOpacity style={styles.searchButton}>
               <FontAwesome name="search" size={50} color="#57C5B6" />
             </TouchableOpacity>
@@ -21,15 +23,26 @@ const FamilyInfo = () =>{
       </View>
         <View style = {styles.body}>
           <View style = {styles.familyLine}>
-            <Text style = {styles.familyName}>이숙자(32세)     자녀</Text></View>
+            <Text style = {styles.familyName}>이숙자(32세)     자녀</Text>
+            <TouchableOpacity style={styles.minusButton}>
+             <FontAwesome name="minus-square" size={40} color="#e6e9ed" />
+            </TouchableOpacity>
+          </View>
             <View style={styles.subContainer}>
-                    <TouchableOpacity
-                        onPress={() => navigation.navigate("")}
-                        style={styles.addBtn}
-                    >
-                        <Text style={styles.add}>가족추가하기</Text>
-                    </TouchableOpacity>
-                </View> 
+            <ImageBackground
+              style={{
+              height: 200,
+              width: 200,
+             }}
+              resizeMode="contain"
+              source={require("../../assets/메인로고.png")}
+              />
+              <TouchableOpacity
+                onPress={() => navigation.navigate("")}
+                style={styles.addBtn}
+              ><Text style={styles.add}>가족추가하기</Text>
+              </TouchableOpacity>
+          </View>
         </View>
         <View style={styles.footer}>
           <SearchTabBar />
@@ -48,30 +61,20 @@ const FamilyInfo = () =>{
       backgroundColor: "white",
       flexDirection: "row",
     },
+    backButton:{
+      fontWeight: 500,
+    },
     searchBar: {
       flex: 1,
-      height: "70%",
+      heighft: "70%",
       width: "100%",
       justifyContent: "center",
       alignItems: "center",
       flexDirection: "row",
     },
-    inputText: {
-      fontSize: 18,
-      width: "70%",
-      backgroundColor: "#fafafa",
-      padding: "4%",
-      margin: "3%",
-      borderRadius: 10,
-      marginVertical: "3%",
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
     searchButton: {
       alignSelf: "flex-end",
-      marginBottom: "4%",
+      marginBottom: "5%",
       shadowColor: "#000",
       shadowOffset: {
         width: 0,
@@ -80,21 +83,12 @@ const FamilyInfo = () =>{
       shadowOpacity: 0.2,
       shadowRadius: 3.84,
     },
-  },
     guestName:{
       color:"black",
-      fontSize: 25,
+      fontSize: 23,
       fontWeight: 600,
-      marginRight: "10%",
-      alignItems: "center",
-    },
-    guestInFo:{
-      color:"white",
-      fontSize: 20,
-      fontWeight: 600,
-      alignItems: "center",
-      marginLeft: 10,
-      marginTop: 10,
+      marginLeft: "2%",
+      marginRight: "5%",
     },
     body: {
       flex: 6,
@@ -103,15 +97,26 @@ const FamilyInfo = () =>{
     familyLine:{
       flex: 0.25,
       flexDirection: "row",
-      backgroundColor: "#159895",
+      backgroundColor: "#57C5B6",
     },
     familyName:{
       color: "white",
       fontSize: 20,
-      fontWeight: 600,
+      fontWeight: "bold",
       flexDirection: "row",
       alignItems: "center",
       marginTop: 30,
+      marginLeft: "5%",
+    },
+    minusButton: {
+      alignSelf: "flex-end",
+      marginBottom: "7%",
+      marginLeft: "30%",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2,  
+      }
     },
     subContainer: {
       marginTop: "15%",
@@ -125,7 +130,7 @@ const FamilyInfo = () =>{
       height: 50,
       justifyContent: "center",
       alignItems: "center",
-      marginTop: 300,
+      marginTop: 100,
       borderRadius: 10,
     },
     add: {
