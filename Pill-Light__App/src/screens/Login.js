e5db3d3640c9ed6a4190df98ee150b9e9c98cfe5
m2na7 <minha3082@naver.com>
2023-05-17 16:11:58 +0900

    feat: AsyncStorage 이용 회원정보 저장 기능 및 로그인 구현

--- Pill-Light__App/src/screens/Login.js
+++ Pill-Light__App/src/screens/Login.js
@@ -3,21 +3,37 @@ import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View, Keyb
 import React from 'react';
 import { useState } from 'react';
 import { UserInfo } from '../store/UserInfo';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleLoginPress = () => {
-        const loginSuccess = UserInfo(username, password);
+    const handleLogin = async () => {
+        // 로그인 처리 로직을 구현합니다.
+        // AsyncStorage에서 저장된 회원 정보와 비교합니다.
 
-        if (loginSuccess) {
-            Alert.alert('로그인 성공');
-        } else {
-            Alert.alert('로그인 실패', '아이디 또는 비밀번호가 잘못되었습니다.');
+        try {
+            // AsyncStorage에서 저장된 회원 정보 가져오기
+            const storedUsers = await AsyncStorage.getItem('registeredUsers');
+            const parsedUsers = storedUsers ? JSON.parse(storedUsers) : [];
+
+            // 입력된 아이디와 비밀번호와 일치하는 회원 찾기
+            const user = parsedUsers.find(
+                (user) => user.username === username && user.password === password
+            );
+
+            if (user) {
+                Alert.alert('로그인 성공', `로그인한 아이디: ${user.username}`);
+                // 로그인에 성공했을 때, 추가적인 동작을 수행하거나 다른 화면으로 이동할 수 있습니다.
+            } else {
+                Alert.alert('로그인 실패', '아이디 또는 비밀번호가 잘못되었습니다.');
+            }
+        } catch (error) {
+            Alert.alert('로그인 실패', '로그인 중에 오류가 발생했습니다.');
         }
     };
-
     return (
         <SafeAreaView style={styles.safeArea}>
             <KeyboardAvoidingView
@@ -61,7 +77,7 @@ const Login = () => {
                         <View style={styles.subContainer}>
                             <TouchableOpacity
                                 onPress={() => navigation.navigate("Login")}
-                                onPress={handleLoginPress}
+                                onPress={handleLogin}
                                 style={styles.logintBtn}
                             >
                                 <Text style={styles.login}>로그인 하기</Text>
