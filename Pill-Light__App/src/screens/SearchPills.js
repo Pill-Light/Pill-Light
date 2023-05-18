a0bca1b2b518ba81044267a4b0df4dbae61f42b9
SunwooJaeho <118053865+SunwooJaeho@users.noreply.github.com>
2023-05-18 14:23:25 +0900

    design: css 수정

--- Pill-Light__App/src/screens/SearchPills.js
+++ Pill-Light__App/src/screens/SearchPills.js
@@ -16,7 +16,7 @@ const SearchPills = () => {
             style={styles.inputText}
           ></TextInput>
           <TouchableOpacity style={styles.searchButton}>
-            <FontAwesome name="search" size={56} color="#fff" />
+            <FontAwesome name="search" size={56} color="#57C5B6" />
           </TouchableOpacity>
         </View>
       </View>
@@ -31,7 +31,7 @@ const SearchPills = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#57C5B6",
+    backgroundColor: "#fff",
   },
   header: {
     flex: 1,
