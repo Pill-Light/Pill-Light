5a1b5d9081e41a76525a366cce39f0ce9e73fc4b
Soohyeon02 <tngusr@naver.com>
2023-05-19 16:34:02 +0900

    feat: 슬라이드 dots 구현

--- Pill-Light__App/src/screens/MyPill.js
+++ Pill-Light__App/src/screens/MyPill.js
@@ -7,77 +7,90 @@ import TimePickerExample from '../components/TimePickerExample';
 //하단바
 import { Entypo } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
+
 const windowWidth = Dimensions.get('window').width;
-const windowheight = Dimensions.get('window').height;
+const windowHeight = Dimensions.get('window').height;
 
 const MyPill = ({ navigation }) => {
-
   const [isEnabled, setIsEnabled] = useState(false);
   const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  const [currentPage, setCurrentPage] = useState(0);
 
   const handleSlidePress = (key) => {
-    navigation.navigate("MyPillDetail", {pillKey: key});
+    navigation.navigate("MyPillDetail", { pillKey: key });
   };
 
-    return (
-        <SafeAreaView style={styles.container}>
-          {/* header */}
-          <View style={styles.header}>
-            <View style={styles.mainTextWrapper}>
-              <Text style={styles.mainText}>복용 중인 약</Text>
-            </View>
-          </View>
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* header */}
+      <View style={styles.header}>
+        <View style={styles.mainTextWrapper}>
+          <Text style={styles.mainText}>복용 중인 약</Text>
+        </View>
+      </View>
 
+      {/* body */}
+      <View style={styles.body}>
+        {/* carousel */}
+        <ScrollView
+          horizontal
+          pagingEnabled
+          contentContainerStyle={styles.scrollWrapper}
+          showsHorizontalScrollIndicator={false}
+          onScroll={(event) => {
+            const offsetX = event.nativeEvent.contentOffset.x;
+            const pageIndex = Math.round(offsetX / windowWidth);
+            setCurrentPage(pageIndex);
+          }}
+          scrollEventThrottle={200}
+        >
+          {PillData.map((item, index) => {
+            return (
+              <View style={styles.slide} key={item.key}>
+                {/* 시간이랑 스위치 들어가는 부분인 pillOption */}
+                <View style={styles.pillOption}>
+                  <View>
+                    <TimePickerExample style={{ transform: [{ scaleX: 4 }, { scaleY: 4 }] }} />
+                  </View>
+                  <Switch
+                    style={{ transform: [{ scaleX: 2 }, { scaleY: 2 }] }}
+                    onValueChange={toggleSwitch}
+                    value={isEnabled}
+                  />
+                </View>
 
+                {/* 촬영한 알약 이미지 들어가는 부분인 pillImageWrapper */}
+                <TouchableOpacity
+                  onPress={() => handleSlidePress(item.key)}
+                  style={styles.pillImageWrapper}
+                >
+                  <ImageBackground
+                    style={styles.pillImage}
+                    resizeMode="stretch"
+                    source={item.image}
+                  />
+                </TouchableOpacity>
+              </View>
+            );
+          })}
+        </ScrollView>
 
-          {/* body */}
-          <View style={styles.body}>
+        {/* dots */}
+        <View style={styles.dotsContainer}>
+          {PillData.map((_, index) => (
+            <TouchableOpacity
+              key={index}
+              style={[
+                styles.dot,
+                index === currentPage ? styles.activeDot : styles.inactiveDot,
+              ]}
+              onPress={() => setCurrentPage(index)}
+            />
+          ))}
+        </View>
+      </View>
 
-            {/* carousel */}
-            <ScrollView
-              horizontal
-              pagingEnabled
-              contentContainerStyle={styles.scrollWrapper}
-              showsHorizontalScrollIndicator={false}>
-                {PillData.map(item =>{
-                  return (
-
-                    <View style={styles.slide} key={item.key}>
-
-                      {/* 시간이랑 스위치 들어가는 부분인 pillOption */}
-                      <View style={styles.pillOption}>
-                        <View >
-                          <TimePickerExample
-                          style={{ transform: [{ scaleX: 4 }, { scaleY: 4 }]}}/>
-                        </View>
-                        <Switch
-                          style={{ transform: [{ scaleX: 2 }, { scaleY: 2 }]}}
-                          onValueChange={toggleSwitch}
-                          value={isEnabled}
-                        />
-                      </View>
-                      
-                      {/* 촬영한 알약 이미지 들어가는 부분인 pillImageWrapper */}
-                      <TouchableOpacity
-                        onPress={() => handleSlidePress(item.key)}
-                        style={styles.pillImageWrapper}
-                      >
-                        <ImageBackground 
-                          style={styles.pillImage}
-                          resizeMode="stretch"
-                          source={item.image}>
-                        </ImageBackground>
-                      </TouchableOpacity>
-                    </View>
-
-                  )
-                })}
-            </ScrollView>
-          </View>
-
-
-
-          {/* 하단바 */}
+      {/* 하단바 */}
       <View style={styles.bottom}>
         <View style={styles.camera}>
           <Entypo name="camera" size={75} color="black" />
@@ -109,8 +122,8 @@ const styles = StyleSheet.create({
 
   body: {
     flex: 4,
-    backgroundColor: "blue",
-    alignContent: "center"
+    backgroundColor: "white",
+    alignContent: "center",
   },
   scrollWrapper: {
     backgroundColor: "red",
@@ -138,6 +151,26 @@ const styles = StyleSheet.create({
     height: "100%",
     alignItems: "center",
   },
+
+  dotsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 15,
+  },
+  dot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginHorizontal: 5,
+  },
+  activeDot: {
+    backgroundColor: 'skyblue', // 현재 페이지 dot의 색상을 설정합니다.
+  },
+  inactiveDot: {
+    backgroundColor: '#e9e9e9', // 비활성화된 dot의 색상을 설정합니다.
+  },
+  
 
   bottom: {
     flex: 1,
